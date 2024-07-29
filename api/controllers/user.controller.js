1d764841ce3a2ee035935e0a6d7c92ddc09d638e
Labib-Ahammed <labibahammed087@gmail.com>
2024-07-29 16:25:03 +0600

    complete delete user funtionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -13,8 +13,8 @@ export const updateUser = async (req, res, next) => {
     if (req.body.password.length < 6) {
       return next(errorHandler(400, "Password must be at least 6 characters"));
     }
-
-    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    const salt = await bcryptjs.genSalt(10);
+    req.body.password = bcryptjs.hashSync(req.body.password, salt);
   }
   if (req.body.username) {
     if (req.body.username.length < 3 || req.body.username.length > 20) {
@@ -54,14 +54,14 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async(req, res, next) =>{
-  if(req.user.id !== req.params.userId){
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
-    await User.findByIdAndDelete(req.params.userId)
-    res.status(200).json('User has been deleted')
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};

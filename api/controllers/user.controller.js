3aaf919563d68a247a7ee1010ea0d7203818d9f5
Labib-Ahammed <labibahammed087@gmail.com>
2024-07-29 11:17:03 +0600

    Complete profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
 export const test = (req, res) => {
   res.json({ message: "API working fine" });
 };
@@ -33,23 +33,23 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, "Username can only contain letters and numbers")
       );
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            profilePicture: req.body.profilePicture,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
         },
-        { new: true }
-      );
-      const {password, ...rest} = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
